b2ec56dcfbd0f1deaa8e9388dffecbc48a3caa91
Carl <carlbrainegebe@gmail.com>
2021-05-20 11:42:20 -0500

    commit tree 3c0e8c2a281ff257acedd7e0c490762d737ea889

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    const firstTwoDrivers = [];
+    firstTwoDrivers.unshift(arrayOfDrivers[0], arrayOfDrivers[1]);
+    
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = function(drivers) {
+   
+    const lastTwoDriversArray = [];
+    lastTwoDriversArray.unshift(drivers[drivers.length - 1]); 
+    lastTwoDriversArray.unshift(drivers[drivers.length - 2]);
+
+    return lastTwoDriversArray;
+}
+
+const selectingDrivers = [];
+selectingDrivers.unshift(returnFirstTwoDrivers);
+selectingDrivers.push(returnLastTwoDrivers);
+
+function createFareMultiplier(fare) {
+    function multiplier(num) {return num * fare;};
+
+    return multiplier;
+}
+
+function fareDoubler(fare) {
+
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    
+   //return the first two drivers
+return returnFirstTwoDrivers(drivers);
+   
+}
